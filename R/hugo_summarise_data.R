a7ffe813e8e22a368006bd6d7453e737f19a3fca
KasiaWo <32644095+KasiaWo@users.noreply.github.com>
2018-06-12 21:14:31 +0200

    upload

--- R/hugo_summarise_data.R
+++ R/hugo_summarise_data.R
@@ -1,57 +1,35 @@
-#' Create report with variables summary in dataset
+#' Create report with summary of variables in dataset.
 #'
 #' Creates  the report 'html' or 'pdf' in the directory chosen at the beginning of the current
 #' Hugo Investigation in the subfolder 'gallery' in .rda file and a summary in .md file.
-#' Make a data overview report that summarizes the contents of a dataset
-#' and flags potential problems. The potential problems are identified by
-#' running a set of class-specific validation checks, so that different
-#' checks are performed on different variables types.
+#' This reports summarizes the contents of a dataset
+#' and flags potential problems.
+#' To generate report is used function \code{dataMaid::makeDataReport}.
 #'
-#' @param data The dataset to be summarised. This dataset should be of class \code{data.frame},
-#' \code{tibble} or \code{matrix}. If it is of classs \code{matrix}, it will be converted to a
-#' \code{data.frame}.
-#' @param earlier_settings A logical. If \code{TRUE} (the default) and this function was used in investigation, saved parameters will be used.
-#' Parameters given by user are overwrite by saved arguments.
-#' @param output Output format. Options are \code{"pdf"}, \code{"word"} (.docx) and \code{"html"}. This parameters If \code{NULL} (the default),
-#' the output format depends two sequential checks. First, whether a LaTeX installation is available,
-#' in which case \code{pdf} output is chosen. Secondly, if no LaTeX installation
-#' is found, then if the operating system is Windows, \code{word} output is used. Lastly, if neither of these
-#' checks are positive, \code{html} output is used.
-#' @param mode Vector of tasks to perform among the three categories "summarize", "visualize" and "check".
-#' The default, \code{c("summarize", "visualize", "check")}, implies that all three steps are
-#' performed. The steps selected in \code{mode} will be performed for each variable in
-#' \code{data} and their results are presented in the second part of the outputtet data report.
-#' The "summarize" step is responsible for creating the summary table,
-#' the "visualize" step is responsible for creating the plot and the "check" step is responsible
-#' for performing checks on the variable and printing the results if any problems are found.
-#' @param report_title A text string. If supplied, this will be the printed title of the
-#' report. If left unspecified, the title with the name of the supplied dataset.
+#' This function is interactive with user. With first usage user has to type parameters, in next usage user
+#' can choose previous settings or type new. User can change filename and title of report.
+#' User can change output format (\code{output}), decide to overwrite output (\code{replace_output}),
+#'  type column names used to summary (\code{variables}),  decide whether numeric and integer variables with
+#' less than 5 unique values are treated as factor variables (\code{smart_factor}),
+#' type max decimals (\code{max_decimals}) and decide to automatically open report (\code{open_result}).
+#'
+#' @param data R dataset which you wish to summary. This dataset should be of class \code{data.frame},
+#' \code{tibble} or \code{matrix}.
+#' @param overwrite_parameters A logical. If \code{TRUE} (the default) parameters used to generate report will be saved.
 #'
 #' @return The function does not return anything. Its side effect (the production
 #' of a data report) is the reason for running the function.
 #' @importFrom dataMaid makeDataReport
 #' @export
 #'
-#' @examples
-hugo_summarise_data <- function(data, earlier_settings = TRUE,
-                                output = "pdf",
-                                replace = TRUE,
-                                mode = "check",
-                                report_title = NULL){
+hugo_summarise_data <- function(data,
+                                overwrite_params = TRUE
+){
 
-  # argument overwrite_parameters
+  .hugoEnv$history[length(.hugoEnv$history)+1]<-deparse(match.call())
 
-  # zmiana nazwy outputu
-
-  # jesli was_used == T i earlier_settings = True komunikat o nadpisaniu
-
-  #checking whether is correct default_settings
-  # pomysl: funkcja moze sprawdzac ile jest zmiennych i jesli jest ich duzo zapytac czy na pewno
-  #chce podsumowac  wszystkie
-
-  # sprawdzenie typow kolumn, jesli nie wszystkie sa obslugiwane to zapyta uzytkownika jak je zrzutowac
-
-  add_to_history("hugo_summarise_data")
+  stopifnot(class(data) %in% c("data.frame", "tibble", "matrix"))
+  stopifnot(is.logical(overwrite_params))
 
 
   if (!requireNamespace("dataMaid", quietly = TRUE)) {
@@ -60,135 +38,168 @@ hugo_summarise_data <- function(data, earlier_settings = TRUE,
          call. = FALSE)
   }
 
-  cat('hugo: I\'m trying to prepare report for you. \n')
 
-  stopifnot(is.logical(earlier_settings))
+  data_name <- deparse(substitute(data))
+  if (substr(data_name, 1, 11) == "data.frame(") {
+    data_name <- "unnamed_data"
+  }
 
-  if (earlier_settings){
-    was_used <- check_history(data)
-    print(was_used)
-    if(!was_used){
-      if(missing(earlier_settings)){
-        answer <- readline("This is the first usage of this function.\n You choose to use default settings for generate report. Are you sure? y/n")
-        if(! answer %in% c("y","n") ){
-          cat("Incorrect value. We have to stop generate report.")
-        }
-        if( answer == "n") {
-          stop(" We have to stop generate report.")
+  default_filename <-  paste0( "hugo_summary_", data_name)
 
-        }
-        if( answer == "y") {
-          cat(" We will generate report with default settings.")
-
-        }
-
-      }
+  path <- normalizePath(paste0("./",.hugoEnv$path, "/gallery"))
+    if (!dir.exists(path)) {
+    dir.create(path)
     }
 
+  print(path)
+
+  columns_name <- colnames(data)
+
+  cat(' Hugo\'s trying to prepare report for you. \n')
+
+  if(!getOption("hugo.know_summary_parameters" )){
+
+    default_parameters <- get_parameters_from_user(columns_name)
+    filename <- filename_input(default_filename, path)
+    report_title <- title_input(data_name)
+  }
+   else{
+     default_parameters <- .hugoEnv$summarise_data_default_params
+     print("wczytane domyslne parametry")
+     print(default_parameters)
+     hugo_menu <- menu_prev_settings(default_parameters)
+
+     if(is.null(hugo_menu)) {return()}
+     if(!hugo_menu) {
+
+       default_parameters <- get_parameters_from_user(columns_name)
+     }
+
+      filename <- filename_input(default_filename, path = path)
+      report_title <- title_input(data_name)
+
+
+   }
+  options(hugo.know_summary_parameters = T)
+
+
+  if(overwrite_params){
+    save_parametrs(default_parameters)
   }
 
-  else was_used <- FALSE
-  # path <- paste0(.hugoEnv$path, "/gallery")
-  # if (!dir.exists(path)) {
-  #   dir.create(path)
-  # }
+
+  parameters <- c(list(data = quote(data), reportTitle = report_title,
+                       file = filename, codebook = TRUE,
+                       quiet = "silent"), default_parameters )
+  print(default_parameters)
 
 
+  results <- tryCatch({suppressWarnings(do.call('makeDataReport',
+                                                args = parameters
+  ))},
+  warning = function(w) {
+    warning("While generating a report: ", w)
+  }, error = function(e) {
+    e$message <- paste0("While generating a report: ", e)
+    stop(e)
+  })
 
-
-
-  parameters <- get_parameters(data, earlier_settings = earlier_settings, was_used,
-                               output = output,
-                               replace = replace,
-                               report_title = report_title,
-                               mode = mode)
-
-  print(parameters)
-
-  resluts <- tryCatch({do.call(eval(parse(text='dataMaid::makeDataReport')),args = parameters )},
-                      warning = function(w) {
-                        warning("While generating a report: ", w)
-                      }, error = function(e) {
-                        e$message <- paste0("While generating a report: ", e)
-                        stop(e)
-                      })
-  # resluts <- tryCatch({dataMaid:::makeDataReport(data = parameters[[1]],
-  #                                               output = parameters[[2]],
-  #                                               reportTitle = parameters[[3]],
-  #                                               mode = parameters[[4]])},
-  #                     warning = function(w) {
-  #                       warning("While generating a report: ", w)
-  #                     }, error = function(e) {
-  #                       e$message <- paste0("While generating a report: ", e)
-  #                       stop(e)
-  #                     })
-  # zmiana bledow
-
-  save_parametrs(parameters)
-  #
-  #return(parameters)
-}
-
-
-# this function check whether function was used in history
-check_history <- function(data, function_name = "hugo_summarise_data"){
-
-  index <- any(grepl(function_name, .hugoEnv$history[1:(length(.hugoEnv$history)-1)]))
-
-  return(index)
-}
-
-
-
-
-get_parameters <- function(data, earlier_settings ,was_used = FALSE, output , replace, report_title , mode  ){
-  if(was_used &  earlier_settings){
-    #list_files <- list.files(".")
-    if(exists("summarise_data_default_params", envir = .hugoEnv))
-      parameters <- .hugoEnv$summarise_data_default_params
-    else{
-      cat("Default parameters are not found in directory. Default settings will be used.\n")
-      parameters <- create_parameters_list(data = quote(data), output = output,
-                                           replace = replace,
-                                           report_title = report_title,
-                                           mode = mode)
-    }
+  cat("Success!")
 
   }
-  else{
-    parameters <- create_parameters_list(data = quote(data),
-                                         output = output,
-                                         replace = replace,
-                                         report_title = report_title,
-                                         mode = mode)
+
+
+
+get_parameters_from_user <- function(columns_name){
+  cat("Please insert output format: > ",file = getOption('hugo.connection_out'))
+  output <- readLines(con = getOption('hugo.connection_in'), n = 1)
+  if(!output %in% c("pdf", "html", "")){
+    cat("Incorrect format of output. Default html.\n")
+    output <- "html"
   }
-  return(parameters)
+
+  cat("Do you want to replace existing file: > ",file = getOption('hugo.connection_out'))
+  replace_output <- as.logical(readLines(con = getOption('hugo.connection_in'), n = 1))
+  if(is.na(replace_output)){
+    cat("Incorrect value. Default is TRUE.\n")
+    replace_output <- TRUE
+  }
+
+  cat("Do you want to open output file: > ",file = getOption('hugo.connection_out'))
+  open_output <- as.logical(readLines(con = getOption('hugo.connection_in'), n = 1))
+  if(is.na(open_output)){
+    cat("Incorrect value. Default is TRUE.\n")
+    replace_output <- TRUE
+  }
+
+  cat(paste0("Please type vector of column names which you want to summarise \n (in data are ", length(columns_name), " columns, to use all type \"all\"): > "),file = getOption('hugo.connection_out'))
+  variables <- readLines(con = getOption('hugo.connection_in'), n = 1)
+  if(variables == "all") variables <- NULL
+
+
+
+  cat("Do you want to numeric colums with less than 5 unique values are treated as factor variables?: > ",file = getOption('hugo.connection_out'))
+  smart_factor <- as.logical(readLines(con = getOption('hugo.connection_in'), n = 1))
+  if(!is.na(replace_output)){
+    cat("Incorrect value. Default is TRUE.\n")
+    smart_factor <- TRUE
+  }
+
+  cat("Please type max decimals: > ",file = getOption('hugo.connection_out'))
+  max_decimals <- suppressWarnings(as.numeric(readLines(con = getOption('hugo.connection_in'), n = 1)))
+
+  if(! (max_decimals %% 1 == 0) | is.na(max_decimals) ){
+    cat("Incorrect value. Default is 2.\n")
+    max_decimals <- TRUE
+  }
+
+  return(list(output = output,
+              replace = replace_output,
+              useVars = variables ,
+              smartNum = smart_factor,
+              maxDecimals = max_decimals,
+              openResult = open_output))
+}
+
+menu_prev_settings <- function(default_parameters){
+  hugo_menu <- switch(utils::menu(c('Use previous settings for summary.','Enter new settings.'),
+                                  title = paste0("Found previous settings:  ",
+                                                 paste(names(default_parameters),"=", unlist(as.character(default_parameters)), collapse = ", ")
+                                  )),T,F)
+  return(hugo_menu)
 }
 
 
+filename_input <- function(default_filename, path){
 
+  cat(paste0("To use \" ", default_filename, "\" name type NULL or type your name of output (without path): > "), file = getOption('hugo.connection_out'))
+  filename <- readLines(con = getOption('hugo.connection_in'), n = 1)
+  if(! filename %in% c("NULL", "")){
+    filename <- paste0(path, "/",  basename(tools::file_path_sans_ext(filename)), ".Rmd")
+  }
+  else
+    filename <- paste0(path, "/hugo_summary_",  default_filename, ".Rmd")
 
-create_parameters_list <- function(data, output = NULL, replace, report_title = NULL, mode){
-  default_parameters<- list(data = quote(data),
-                             output = output,
-                              replace = replace,
-                             reportTitle = report_title,
-                             mode = mode)
-
-  #additional_parameters <- list(...)
-  # komunikat: parameters from ... will be overriden by default_parameters
-
-  default_parameters
+  return(filename)
 }
+
+title_input <- function(data_name){
+
+  cat(paste0("To use \" ", data_name, "\" as title type NULL or type your report title: > "),file = getOption('hugo.connection_out'))
+  report_title <- readLines(con = getOption('hugo.connection_in'), n = 1)
+  if(!report_title %in% c("NULL", "")){
+    report_title <- default_filename
+  }
+
+  return(report_title)
+}
+
+
 
 
 save_parametrs <- function(parameters){
-  # if(exists("summarise_data_default_params", envir = .hugoEnv)) {
-  #   assign('summarise_data_default_params',parameters, envir = .hugoEnv)
-  # }
-  # if(!exists("summarise_data_default_params", envir = .hugoEnv)) {
-  #   assign('summarise_data_default_params',parameters, envir = .hugoEnv)
-  # }
+
 
   .hugoEnv$summarise_data_default_params <- parameters
 }
+
