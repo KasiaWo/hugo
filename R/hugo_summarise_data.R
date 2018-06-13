c1a1317220914db0ad7b79170e8f15e379b934fe
KasiaWo <32644095+KasiaWo@users.noreply.github.com>
2018-06-13 07:46:45 +0200

    upload summarise

--- R/hugo_summarise_data.R
+++ R/hugo_summarise_data.R
@@ -9,16 +9,20 @@
 #' This function is interactive with user. With first usage user has to type parameters, in next usage user
 #' can choose previous settings or type new. User can change filename and title of report.
 #' User can change output format (\code{output}), decide to overwrite output (\code{replace_output}),
-#'  type column names used to summary (\code{variables}),  decide whether numeric and integer variables with
+#'   decide whether numeric and integer variables with
 #' less than 5 unique values are treated as factor variables (\code{smart_factor}),
 #' type max decimals (\code{max_decimals}) and decide to automatically open report (\code{open_result}).
 #'
 #' @param data R dataset which you wish to summary. This dataset should be of class \code{data.frame},
 #' \code{tibble} or \code{matrix}.
-#' @param overwrite_parameters A logical. If \code{TRUE} (the default) parameters used to generate report will be saved.
+#' @param overwrite_params logical. If \code{TRUE} (the default) parameters used to generate report will be saved.
 #'
 #' @return The function does not return anything. Its side effect (the production
 #' of a data report) is the reason for running the function.
+#' @example
+#' \dontrun{
+#' hugo_summarise_data(iris)
+#' }
 #' @importFrom dataMaid makeDataReport
 #' @export
 #'
@@ -51,13 +55,13 @@ hugo_summarise_data <- function(data,
     dir.create(path)
     }
 
-  print(path)
+
 
   columns_name <- colnames(data)
 
   cat(' Hugo\'s trying to prepare report for you. \n')
 
-  if(!getOption("hugo.know_summary_parameters" )){
+  if(!getOption("hugo.know_summary_parameters"  )){
 
     default_parameters <- get_parameters_from_user(columns_name)
     filename <- filename_input(default_filename, path)
@@ -65,8 +69,7 @@ hugo_summarise_data <- function(data,
   }
    else{
      default_parameters <- .hugoEnv$summarise_data_default_params
-     print("wczytane domyslne parametry")
-     print(default_parameters)
+
      hugo_menu <- menu_prev_settings(default_parameters)
 
      if(is.null(hugo_menu)) {return()}
@@ -80,18 +83,16 @@ hugo_summarise_data <- function(data,
 
 
    }
-  options(hugo.know_summary_parameters = T)
 
 
-  if(overwrite_params){
-    save_parametrs(default_parameters)
-  }
 
 
+
+  filepath <- paste0(path, "/",  filename, ".Rmd")
   parameters <- c(list(data = quote(data), reportTitle = report_title,
-                       file = filename, codebook = TRUE,
+                       file = filepath , codebook = TRUE,
                        quiet = "silent"), default_parameters )
-  print(default_parameters)
+
 
 
   results <- tryCatch({suppressWarnings(do.call('makeDataReport',
@@ -103,6 +104,13 @@ hugo_summarise_data <- function(data,
     e$message <- paste0("While generating a report: ", e)
     stop(e)
   })
+  add_path_to_history(paste0(.hugoEnv$path, "/gallery/",filename,".", parameters$output))
+
+  if(overwrite_params){
+    save_parameters(default_parameters)
+    options(hugo.know_summary_parameters = T)
+                      }
+
 
   cat("Success!")
 
@@ -110,52 +118,45 @@ hugo_summarise_data <- function(data,
 
 
 
+
 get_parameters_from_user <- function(columns_name){
-  cat("Please insert output format: > ",file = getOption('hugo.connection_out'))
-  output <- readLines(con = getOption('hugo.connection_in'), n = 1)
-  if(!output %in% c("pdf", "html", "")){
-    cat("Incorrect format of output. Default html.\n")
-    output <- "html"
+
+  output <- get_output_from_user()
+  if(is.null(output)) {
+    output <- 'html'
+    message("Default value will be used: html")
   }
 
-  cat("Do you want to replace existing file: > ",file = getOption('hugo.connection_out'))
-  replace_output <- as.logical(readLines(con = getOption('hugo.connection_in'), n = 1))
-  if(is.na(replace_output)){
-    cat("Incorrect value. Default is TRUE.\n")
+  replace_output <- get_replace_from_user()
+  if(is.null(replace_output)) {
     replace_output <- TRUE
+    message("Default value will be used: TRUE")
+  }
+  open_output <- get_open_from_user()
+  if(is.null(open_output)) {
+    open_output <- FALSE
+    message("Default value will be used: FALSE")
   }
 
-  cat("Do you want to open output file: > ",file = getOption('hugo.connection_out'))
-  open_output <- as.logical(readLines(con = getOption('hugo.connection_in'), n = 1))
-  if(is.na(open_output)){
-    cat("Incorrect value. Default is TRUE.\n")
-    replace_output <- TRUE
-  }
 
-  cat(paste0("Please type vector of column names which you want to summarise \n (in data are ", length(columns_name), " columns, to use all type \"all\"): > "),file = getOption('hugo.connection_out'))
-  variables <- readLines(con = getOption('hugo.connection_in'), n = 1)
-  if(variables == "all") variables <- NULL
-
-
-
-  cat("Do you want to numeric colums with less than 5 unique values are treated as factor variables?: > ",file = getOption('hugo.connection_out'))
-  smart_factor <- as.logical(readLines(con = getOption('hugo.connection_in'), n = 1))
-  if(!is.na(replace_output)){
-    cat("Incorrect value. Default is TRUE.\n")
+  smart_factor <- get_smart_factor_from_user()
+  if(is.null(smart_factor)) {
     smart_factor <- TRUE
+    message("Default value will be used: TRUE")
   }
+
+
 
   cat("Please type max decimals: > ",file = getOption('hugo.connection_out'))
   max_decimals <- suppressWarnings(as.numeric(readLines(con = getOption('hugo.connection_in'), n = 1)))
 
-  if(! (max_decimals %% 1 == 0) | is.na(max_decimals) ){
-    cat("Incorrect value. Default is 2.\n")
+  if((! max_decimals %% 1 == 0) | (is.na(max_decimals)) |( max_decimals > 10)||( max_decimals < 0)){
+    message("Incorrect value. Default 2 will be used.")
     max_decimals <- TRUE
   }
 
   return(list(output = output,
               replace = replace_output,
-              useVars = variables ,
               smartNum = smart_factor,
               maxDecimals = max_decimals,
               openResult = open_output))
@@ -169,26 +170,54 @@ menu_prev_settings <- function(default_parameters){
   return(hugo_menu)
 }
 
+get_output_from_user <- function (){
+  output <- switch(utils::menu(c('pdf','html'),
+                               title = paste0("Please choose output format: > ")),'pdf','html')
+  return(output)
+}
+
+get_replace_from_user <- function (){
+  replace_output <- switch(utils::menu(c('TRUE','FALSE'),
+                               title = paste0('Do you want to replace existing file: >')),TRUE,FALSE)
+  return(replace_output)
+}
+get_open_from_user <- function (){
+  open_output <- switch(utils::menu(c('TRUE','FALSE'),
+                                       title = paste0('Do you want to open output file: > ')),TRUE,FALSE)
+  return(open_output)
+}
+
+get_smart_factor_from_user <- function (){
+  smart_factor <- switch(utils::menu(c('TRUE','FALSE'),
+                                       title = paste0("Do you want to numeric colums with less than 5 unique values are treated as factor variables?: > ")),TRUE,FALSE)
+  return(smart_factor)
+}
+
+
 
 filename_input <- function(default_filename, path){
 
-  cat(paste0("To use \" ", default_filename, "\" name type NULL or type your name of output (without path): > "), file = getOption('hugo.connection_out'))
+  cat(paste0("To use \"", default_filename, "\" name type NULL or type your name of output (without path): > "), file = getOption('hugo.connection_out'))
   filename <- readLines(con = getOption('hugo.connection_in'), n = 1)
   if(! filename %in% c("NULL", "")){
-    filename <- paste0(path, "/",  basename(tools::file_path_sans_ext(filename)), ".Rmd")
-  }
-  else
-    filename <- paste0(path, "/hugo_summary_",  default_filename, ".Rmd")
 
+    filename <-  basename(tools::file_path_sans_ext(filename))
+
+  }
+  else{
+    message("Default filename will be used")
+    filename <-  default_filename
+  }
   return(filename)
 }
 
 title_input <- function(data_name){
 
-  cat(paste0("To use \" ", data_name, "\" as title type NULL or type your report title: > "),file = getOption('hugo.connection_out'))
+  cat(paste0("To use \"", data_name, "\" as title type NULL or type your report title: > "),file = getOption('hugo.connection_out'))
   report_title <- readLines(con = getOption('hugo.connection_in'), n = 1)
-  if(!report_title %in% c("NULL", "")){
-    report_title <- default_filename
+  if(report_title %in% c("NULL", "")){
+    message("Default title will be used")
+    report_title <- data_name
   }
 
   return(report_title)
@@ -197,7 +226,7 @@ title_input <- function(data_name){
 
 
 
-save_parametrs <- function(parameters){
+save_parameters <- function(parameters){
 
 
   .hugoEnv$summarise_data_default_params <- parameters
